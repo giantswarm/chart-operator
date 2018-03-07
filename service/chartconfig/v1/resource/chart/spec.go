9f25c832663a304715980106025dc97be5bd0ec0
Ross Fairbanks <ross@giantswarm.io>
2018-03-07 12:57:10 +0100

    Add GoDoc for ChartState

--- service/chartconfig/v1/resource/chart/spec.go
+++ service/chartconfig/v1/resource/chart/spec.go
@@ -1,7 +1,14 @@
 package chart
 
+// ChartState holds the state of the chart to be reconciled.
 type ChartState struct {
-	ChartName   string
+	// ChartName is the fully qualified name of the Helm Chart.
+	// e.g. quay.io/giantswarm/chart-operator-chart
+	ChartName string
+	// ChannelName is the CNR channel to reconcile against.
+	// e.g. 0.1-beta
 	ChannelName string
+	// ReleaseName is the Helm Release to be deployed.
+	// e.g. 0.1.2
 	ReleaseName string
 }
