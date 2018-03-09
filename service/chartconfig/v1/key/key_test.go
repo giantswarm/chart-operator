25aa21cb5681cf32421651a25cfc737ed5e4b1a2
Ross Fairbanks <ross@giantswarm.io>
2018-03-09 17:22:25 +0100

    Add test for ToCustomObject

--- service/chartconfig/v1/key/key_test.go
+++ service/chartconfig/v1/key/key_test.go
@@ -1,6 +1,7 @@
 package key
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
@@ -37,5 +38,57 @@ func Test_ChannelName(t *testing.T) {
 
 	if ChannelName(obj) != expectedChannel {
 		t.Fatalf("chart name %s, want %s", ChannelName(obj), expectedChannel)
+	}
+}
+
+func Test_ToCustomObject(t *testing.T) {
+	testCases := []struct {
+		name           string
+		input          interface{}
+		expectedObject v1alpha1.ChartConfig
+		errorMatcher   func(error) bool
+	}{
+		{
+			name: "case 0: basic match",
+			input: &v1alpha1.ChartConfig{
+				Spec: v1alpha1.ChartConfigSpec{
+					Chart: v1alpha1.ChartConfigSpecChart{
+						Name:    "chart-operator-chart",
+						Channel: "0.1-beta",
+					},
+				},
+			},
+			expectedObject: v1alpha1.ChartConfig{
+				Spec: v1alpha1.ChartConfigSpec{
+					Chart: v1alpha1.ChartConfigSpecChart{
+						Name:    "chart-operator-chart",
+						Channel: "0.1-beta",
+					},
+				},
+			},
+		},
+		{
+			name:         "case 1: wrong type",
+			input:        &v1alpha1.CertConfig{},
+			errorMatcher: IsWrongTypeError,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := ToCustomObject(tc.input)
+			switch {
+			case err != nil && tc.errorMatcher == nil:
+				t.Fatalf("error == %#v, want nil", err)
+			case err == nil && tc.errorMatcher != nil:
+				t.Fatalf("error == nil, want non-nil")
+			case err != nil && !tc.errorMatcher(err):
+				t.Fatalf("error == %#v, want matching", err)
+			}
+
+			if !reflect.DeepEqual(result, tc.expectedObject) {
+				t.Fatalf("Custom Object == %q, want %q", result, tc.expectedObject)
+			}
+		})
 	}
 }
