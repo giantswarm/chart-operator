e7a183485309f2ec27ffe80efa25556528c5e10e
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-03-09 16:33:00 +0100

    Use latest operatorkit (#14)
    * vendor: update operatorkit
    
    * Use new operatorkit interface

--- service/chartconfig/chart_framework.go
+++ service/chartconfig/chart_framework.go
@@ -43,10 +43,10 @@ func NewChartFramework(config ChartFrameworkConfig) (*framework.Framework, error
 
 	var crdClient *k8scrdclient.CRDClient
 	{
-		c := k8scrdclient.DefaultConfig()
-
-		c.K8sExtClient = config.K8sExtClient
-		c.Logger = config.Logger
+		c := k8scrdclient.Config{
+			K8sExtClient: config.K8sExtClient,
+			Logger:       config.Logger,
+		}
 
 		crdClient, err = k8scrdclient.New(c)
 		if err != nil {
@@ -61,9 +61,12 @@ func NewChartFramework(config ChartFrameworkConfig) (*framework.Framework, error
 
 	var newInformer *informer.Informer
 	{
-		c := informer.DefaultConfig()
+		c := informer.Config{
+			Watcher: config.G8sClient.CoreV1alpha1().ChartConfigs(""),
 
-		c.Watcher = config.G8sClient.CoreV1alpha1().ChartConfigs("")
+			RateWait:     informer.DefaultRateWait,
+			ResyncPeriod: informer.DefaultResyncPeriod,
+		}
 
 		newInformer, err = informer.New(c)
 		if err != nil {
