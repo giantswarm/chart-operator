5010713e0a71cfdf48df77e7b91e5d111885b082
Ross Fairbanks <ross@giantswarm.io>
2018-03-16 18:19:04 +0100

    Implement GetCurrentState using Helm client

--- service/chartconfig/v1/resource/chart/current.go
+++ service/chartconfig/v1/resource/chart/current.go
@@ -2,8 +2,36 @@ package chart
 
 import (
 	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/chart-operator/service/chartconfig/v1/key"
 )
 
 func (r *Resource) GetCurrentState(ctx context.Context, obj interface{}) (interface{}, error) {
-	return nil, nil
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	releaseContent, err := r.helmClient.GetReleaseContent(customObject)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	releaseHistory, err := r.helmClient.GetReleaseHistory(customObject)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	chartState := &ChartState{
+		ChartName:      key.ChartName(customObject),
+		ChannelName:    key.ChannelName(customObject),
+		ReleaseName:    key.ReleaseName(customObject),
+		ChartValues:    releaseContent.Values,
+		ReleaseStatus:  releaseContent.Status,
+		ReleaseVersion: releaseHistory.Version,
+	}
+
+	return chartState, nil
 }
