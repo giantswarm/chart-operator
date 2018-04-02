927540d54e844d5f4c61d62311102e9ff8aab28f
Federico Gimenez <federico@giantswarm.io>
2018-04-02 14:59:52 +0200

    fix helm service creation

--- integration/operator_test.go
+++ integration/operator_test.go
@@ -10,8 +10,11 @@ import (
 	"testing"
 
 	"github.com/giantswarm/e2e-harness/pkg/framework"
+	"github.com/giantswarm/e2e-harness/pkg/harness"
 	"github.com/giantswarm/micrologger"
 	"github.com/spf13/afero"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 
 	"github.com/giantswarm/chart-operator/service/chartconfig/v1/appr"
 	"github.com/giantswarm/chart-operator/service/chartconfig/v1/helm"
@@ -81,39 +84,49 @@ func TestGetReleaseVersion(t *testing.T) {
 func TestInstallChart(t *testing.T) {
 	l, err := micrologger.New(micrologger.Config{})
 	if err != nil {
-		t.Errorf("could not create logger %v", err)
+		t.Fatalf("could not create logger %v", err)
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", harness.DefaultKubeConfig)
+	if err != nil {
+		t.Fatalf("could not create k8s config %v", err)
+	}
+	cs, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		t.Fatalf("could not create k8s client %v", err)
 	}
 
 	hc := helm.Config{
-		Logger: l,
-		Host:   "http://localhost:44134",
+		Logger:     l,
+		K8sClient:  cs,
+		RestConfig: config,
 	}
 
 	h, err := helm.New(hc)
 	if err != nil {
-		t.Errorf("could not create helm client %v", err)
+		t.Fatalf("could not create helm client %v", err)
 	}
 
 	// integration dir is mounted in /e2e in the test container.
 	tarballPath := filepath.Join("/e2e", "tb-chart.tar.gz")
 	err = h.InstallFromTarball(tarballPath, "default")
 	if err != nil {
-		t.Errorf("could not install chart %v", err)
+		t.Fatalf("could not install chart %v", err)
 	}
 
 	releaseContent, err := h.GetReleaseContent("tb-chart")
 	if err != nil {
-		t.Errorf("could not get release content %v", err)
+		t.Fatalf("could not get release content %v", err)
 	}
 	fmt.Printf("release content: %+v\n", releaseContent)
 	/*
 		expected := "5.5.5"
 		actual, err := a.GetReleaseVersion("tb-chart", "5-5-beta")
 		if err != nil {
-			t.Errorf("could not get release %v", err)
+			t.Fatalf("could not get release %v", err)
 		}
 
 		if expected != actual {
-			t.Errorf("release didn't match expected, want %q, got %q", expected, actual)
+			t.Fatalf("release didn't match expected, want %q, got %q", expected, actual)
 		}*/
 }
