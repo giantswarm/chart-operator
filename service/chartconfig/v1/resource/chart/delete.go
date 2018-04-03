65c061f734d4b38903388ffca211879bbd41f76f
Tobias Richter <tr4711@gmail.com>
2018-04-03 15:58:12 +0200

    Add ApplyDeleteChange

--- service/chartconfig/v1/resource/chart/delete.go
+++ service/chartconfig/v1/resource/chart/delete.go
@@ -2,11 +2,36 @@ package chart
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/giantswarm/chart-operator/service/chartconfig/v1/key"
+	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/operatorkit/framework"
 )
 
 func (r *Resource) ApplyDeleteChange(ctx context.Context, obj, deleteChange interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	chartState, err := toChartState(deleteChange)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if chartState.ReleaseName != "" {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting release %s", chartState.ReleaseName))
+		release := key.ReleaseName(customObject)
+
+		err := r.helmClient.DeleteRelease(release)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting release %s", chartState.ReleaseName))
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("not deleting release %s", chartState.ReleaseName))
+	}
 	return nil
 }
 
