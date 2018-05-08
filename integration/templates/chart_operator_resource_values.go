6044ce77c7b234b06ef00947a7606f49d120ac80
Ross Fairbanks <ross@giantswarm.io>
2018-05-08 12:11:30 +0200

    Use correct namespaces

--- integration/templates/chart_operator_resource_values.go
+++ integration/templates/chart_operator_resource_values.go
@@ -6,6 +6,6 @@ package templates
 const ChartOperatorResourceValues = `chart:
   name: "tb-chart"
   channel: "5-5-beta"
-  namespace: "giantswarm"
+  namespace: "default"
   release: "tb-release"
 `
