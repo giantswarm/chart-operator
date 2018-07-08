956f8af5d1dc3d1f6902875310e79e9c663473d1
Ross Fairbanks <ross@giantswarm.io>
2018-07-08 11:35:56 +0200

    Add chartstatus resource

---
+++ service/controller/v2/resource/chartstatus/create.go
@@ -0,0 +1,46 @@
+package chartstatus
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/chart-operator/service/controller/v2/key"
+)
+
+func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	releaseName := key.ReleaseName(customObject)
+	r.logger.LogCtx(ctx, "level", "debug", fmt.Sprintf("getting status for release '%s'", releaseName))
+
+	releaseContent, err := r.helmClient.GetReleaseContent(releaseName)
+	// TODO
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	if key.ReleaseStatus(customObject) != releaseContent.Status {
+		r.logger.LogCtx(ctx, "level", "debug", fmt.Sprintf("setting status for release '%s' status to '%s'", releaseName, releaseContent.Status))
+
+		customObjectCopy := customObject.DeepCopy()
+		customObjectCopy.Status.ReleaseStatus = releaseContent.Status
+
+		namespace := key.Namespace(customObject)
+		r.g8sClient.CoreV1alpha1().ChartConfigs(namespace).UpdateStatus(customObjectCopy)
+		// TODO
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		r.logger.LogCtx(ctx, "level", "debug", fmt.Sprintf("status set for release '%s'", releaseName))
+	} else {
+		r.logger.LogCtx(ctx, "level", "debug", fmt.Sprintf("status for release '%s' already set to '%s'", releaseName, releaseContent.Status))
+	}
+
+	return nil
+}
