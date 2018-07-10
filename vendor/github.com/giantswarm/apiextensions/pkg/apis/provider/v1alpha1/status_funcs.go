04186ccdc40fd2d9ad9865ef0b31e3d5571a93fb
Bricktop <marcel.mueller1@rwth-aachen.de>
2018-07-10 15:48:15 +0200

    Add secret in key package (#88)

---
+++ vendor/github.com/giantswarm/apiextensions/pkg/apis/provider/v1alpha1/status_funcs.go
@@ -0,0 +1,92 @@
+package v1alpha1
+
+func (s StatusCluster) HasCreatedCondition() bool {
+	return hasCondition(s.Conditions, StatusClusterStatusTrue, StatusClusterTypeCreated)
+}
+
+func (s StatusCluster) HasCreatingCondition() bool {
+	return hasCondition(s.Conditions, StatusClusterStatusTrue, StatusClusterTypeCreating)
+}
+
+func (s StatusCluster) HasUpdatedCondition() bool {
+	return hasCondition(s.Conditions, StatusClusterStatusTrue, StatusClusterTypeUpdated)
+}
+
+func (s StatusCluster) HasUpdatingCondition() bool {
+	return hasCondition(s.Conditions, StatusClusterStatusTrue, StatusClusterTypeUpdating)
+}
+
+func (s StatusCluster) HasVersion(semver string) bool {
+	return hasVersion(s.Versions, semver)
+}
+
+func (s StatusCluster) LatestVersion() string {
+	if len(s.Versions) == 0 {
+		return ""
+	}
+
+	latest := s.Versions[0]
+
+	for _, v := range s.Versions {
+		if latest.Date.Before(v.Date) {
+			latest = v
+		}
+	}
+
+	return latest.Semver
+}
+
+func (s StatusCluster) WithCreatedCondition() []StatusClusterCondition {
+	return withCondition(s.Conditions, StatusClusterTypeCreating, StatusClusterTypeCreated, StatusClusterStatusTrue)
+}
+
+func (s StatusCluster) WithCreatingCondition() []StatusClusterCondition {
+	return withCondition(s.Conditions, StatusClusterTypeCreated, StatusClusterTypeCreating, StatusClusterStatusTrue)
+}
+
+func (s StatusCluster) WithUpdatedCondition() []StatusClusterCondition {
+	return withCondition(s.Conditions, StatusClusterTypeUpdating, StatusClusterTypeUpdated, StatusClusterStatusTrue)
+}
+
+func (s StatusCluster) WithUpdatingCondition() []StatusClusterCondition {
+	return withCondition(s.Conditions, StatusClusterTypeUpdated, StatusClusterTypeUpdating, StatusClusterStatusTrue)
+}
+
+func hasCondition(conditions []StatusClusterCondition, search string, status string) bool {
+	for _, c := range conditions {
+		if c.Status == search && c.Type == status {
+			return true
+		}
+	}
+
+	return false
+}
+
+func hasVersion(versions []StatusClusterVersion, search string) bool {
+	for _, v := range versions {
+		if v.Semver == search {
+			return true
+		}
+	}
+
+	return false
+}
+
+func withCondition(conditions []StatusClusterCondition, search string, replace string, status string) []StatusClusterCondition {
+	newConditions := []StatusClusterCondition{
+		{
+			Status: status,
+			Type:   replace,
+		},
+	}
+
+	for _, c := range conditions {
+		if c.Type == search {
+			continue
+		}
+
+		newConditions = append(newConditions, c)
+	}
+
+	return newConditions
+}
