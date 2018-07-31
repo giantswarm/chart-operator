2ea6bcac56de9e42131d68cae19dffb5cc09792a
Ross Fairbanks <ross@giantswarm.io>
2018-07-31 19:36:19 +0200

    Use tiller installed in giantswarm namespace

--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -48,9 +48,10 @@ func TestMain(m *testing.M) {
 
 	{
 		c := helmclient.Config{
-			Logger:     l,
-			K8sClient:  f.K8sClient(),
-			RestConfig: f.RestConfig(),
+			Logger:          l,
+			K8sClient:       f.K8sClient(),
+			RestConfig:      f.RestConfig(),
+			TillerNamespace: "giantswarm",
 		}
 		helmClient, err = helmclient.New(c)
 		if err != nil {
