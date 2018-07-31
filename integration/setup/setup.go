739c0a89cbdea61db067216a2c3b9517b238925e
Ross Fairbanks <ross@giantswarm.io>
2018-07-31 19:36:19 +0200

    Use tiller installed in giantswarm namespace

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -29,6 +29,12 @@ func WrapTestMain(f *framework.Host, helmClient *helmclient.Client, m *testing.M
 		v = 1
 	}
 
+	err = helmClient.EnsureTillerInstalled()
+	if err != nil {
+		log.Printf("%#v\n", err)
+		v = 1
+	}
+
 	err = resources(f, helmClient)
 	if err != nil {
 		log.Printf("%#v\n", err)
