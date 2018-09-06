60529345ee4105b768ca1bf5f1218eb65adbb963
Tobias Richter <tr4711@gmail.com>
2018-09-06 19:54:56 +0200

    Rename chart_configmap_values to resource_chart_values

--- integration/templates/chart_configmap_values.go
+++
@@ -1,9 +0,0 @@
-// +build k8srequired
-
-package templates
-
-// ChartConfigMapValues values required by the chart values configmap
-const ChartConfigMapValues = `valuesJson: '{"config":"test-value"}'
-
-secretJson: '{"secret":"test-secret"}'
-`
---
+++ integration/templates/resource_chart_values.go
@@ -0,0 +1,7 @@
+// +build k8srequired
+
+package templates
+
+// ResourceChartValues values required by the tb-resource-chart
+const ResourceChartValues = `valuesJson: '{"config":"test-value"}'
+`
