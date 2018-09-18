ce1340b443ce1591e6353b13b1d9a7d1f7ba17e2
MarcelMue <marcel.mueller1@rwth-aachen.de>
2018-09-18 11:24:37 +0200

    Cleaning up default values

--- integration/test/chartvalues/main_test.go
+++ integration/test/chartvalues/main_test.go
@@ -33,9 +33,10 @@ func init() {
 
 	{
 		c := framework.HostConfig{
-			Logger:          l,
-			ClusterID:       "someval",
-			VaultToken:      "someval",
+			Logger: l,
+
+			ClusterID:       "n/a",
+			VaultToken:      "n/a",
 			TargetNamespace: "giantswarm",
 		}
 
