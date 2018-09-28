5ca4de59458deb06f75ba303901a5a069cb2b2a8
Federico Gimenez <federico.gimenez@gmail.com>
2018-09-28 15:13:49 +0200

    fix v5 package and imports

--- service/controller/v5/resource/chart/resource.go
+++ service/controller/v5/resource/chart/resource.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	// Name is the identifier of the resource.
-	Name = "chartv4"
+	Name = "chartv5"
 )
 
 // Config represents the configuration used to create a new chart resource.
