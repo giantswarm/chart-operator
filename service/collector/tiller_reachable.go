152c0452b373c871b56627e2166ffe202a536df1
Paweł Kopiczko <kopiczko@gmail.com>
2018-11-30 18:30:39 +0000

    Update service/collector/tiller_reachable.go
    Co-Authored-By: TheoBrigitte <theo.brigitte@gmail.com>

--- service/collector/tiller_reachable.go
+++ service/collector/tiller_reachable.go
@@ -29,7 +29,8 @@ func (c *Collector) collectTillerReachable(ctx context.Context, ch chan<- promet
 	if err == nil && len(chartConfigs) == 0 {
 		// Skip pinging tiller when there is no ChartConfig,
 		// as tiller is only installed when there is at least one ChartConfig to reconcile.
-		c.logger.Log("level", "error", "message", "skip pinging Tiller")
+		c.logger.Log("level", "error", "message", "did not collect Tiller reachability")
+		c.logger.Log("level", "error", "message", "no ChartConfg CRs in the cluster")
 
 		value = 1
 	} else {
