ea00c97aa57ef0381e53710cf462ca0202aa4e89
Theo Brigitte <theo.brigitte@gmail.com>
2018-11-30 14:22:08 +0100

    Skip pinging tiller when there is no ChartConfig

--- service/collector/tiller_reachable.go
+++ service/collector/tiller_reachable.go
@@ -19,16 +19,28 @@ var (
 )
 
 func (c *Collector) collectTillerReachable(ctx context.Context, ch chan<- prometheus.Metric) {
-	c.logger.LogCtx(ctx, "level", "debug", "message", "collecting Tiller reachability")
-
-	err := c.helmClient.PingTiller(ctx)
 	var value float64
-	if err != nil {
-		c.logger.Log("level", "error", "message", "could not ping Tiller", "stack", fmt.Sprintf("%#v", err))
 
-		value = 0
-	} else {
+	c.logger.LogCtx(ctx, "level", "debug", "message", "collecting Tiller reachability")
+	chartConfigs, err := c.getChartConfigs()
+	if err != nil {
+		c.logger.LogCtx(ctx, "level", "error", "message", fmt.Sprintf("could not get ChartConfigs"), "stack", fmt.Sprintf("%#v", err))
+	}
+	if err == nil && len(chartConfigs) <= 0 {
+		// Skip pinging tiller when there is no ChartConfig,
+		// as tiller is only installed when there is at least one ChartConfig to reconcile.
+		c.logger.Log("level", "error", "message", "skip pinging Tiller")
+
 		value = 1
+	} else {
+		err := c.helmClient.PingTiller(ctx)
+		if err != nil {
+			c.logger.Log("level", "error", "message", "could not ping Tiller", "stack", fmt.Sprintf("%#v", err))
+
+			value = 0
+		} else {
+			value = 1
+		}
 	}
 
 	ch <- prometheus.MustNewConstMetric(
