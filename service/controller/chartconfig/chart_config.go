284eb2aa94c48e057fedc301cbe2009416a3b566
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-01-02 10:21:13 +0100

    Rename chart controller to chartconfig (#168)
    * Rename chart controller to chartconfig
    
    * Rename controller
    
    * Move v1
    
    * Move v2
    
    * Move v3
    
    * Move v4
    
    * Move v5
    
    * Rename imports
    
    * Rename controller constant
    
    * Fix status resource names

---
+++ service/controller/chartconfig/chart_config.go
@@ -0,0 +1,210 @@
+package chartconfig
+
+import (
+	"github.com/giantswarm/apiextensions/pkg/apis/core/v1alpha1"
+	"github.com/giantswarm/apiextensions/pkg/clientset/versioned"
+	"github.com/giantswarm/apprclient"
+	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/giantswarm/operatorkit/client/k8scrdclient"
+	"github.com/giantswarm/operatorkit/controller"
+	"github.com/giantswarm/operatorkit/informer"
+	"github.com/spf13/afero"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/kubernetes"
+
+	v1 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v1"
+	v2 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v2"
+	v3 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v3"
+	v4 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v4"
+	v5 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v5"
+)
+
+const chartConfigControllerSuffix = "-chartconfig"
+
+type Config struct {
+	ApprClient   apprclient.Interface
+	Fs           afero.Fs
+	G8sClient    versioned.Interface
+	HelmClient   helmclient.Interface
+	K8sClient    kubernetes.Interface
+	K8sExtClient apiextensionsclient.Interface
+	Logger       micrologger.Logger
+
+	ProjectName    string
+	WatchNamespace string
+}
+
+type ChartConfig struct {
+	*controller.Controller
+}
+
+func NewChartConfig(config Config) (*ChartConfig, error) {
+	var err error
+
+	if config.G8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
+	}
+	if config.K8sExtClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sExtClient must not be empty", config)
+	}
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	if config.ProjectName == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.ProjectName must not be empty", config)
+	}
+
+	var crdClient *k8scrdclient.CRDClient
+	{
+		c := k8scrdclient.Config{
+			K8sExtClient: config.K8sExtClient,
+			Logger:       config.Logger,
+		}
+
+		crdClient, err = k8scrdclient.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var newInformer *informer.Informer
+	{
+		c := informer.Config{
+			Logger:  config.Logger,
+			Watcher: config.G8sClient.CoreV1alpha1().ChartConfigs(config.WatchNamespace),
+
+			RateWait:     informer.DefaultRateWait,
+			ResyncPeriod: informer.DefaultResyncPeriod,
+		}
+
+		newInformer, err = informer.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resourceSetV1 *controller.ResourceSet
+	{
+		c := v1.ResourceSetConfig{
+			ApprClient:  config.ApprClient,
+			Fs:          config.Fs,
+			HelmClient:  config.HelmClient,
+			K8sClient:   config.K8sClient,
+			Logger:      config.Logger,
+			ProjectName: config.ProjectName,
+		}
+
+		resourceSetV1, err = v1.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resourceSetV2 *controller.ResourceSet
+	{
+		c := v2.ResourceSetConfig{
+			ApprClient:  config.ApprClient,
+			Fs:          config.Fs,
+			HelmClient:  config.HelmClient,
+			K8sClient:   config.K8sClient,
+			Logger:      config.Logger,
+			ProjectName: config.ProjectName,
+		}
+
+		resourceSetV2, err = v2.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resourceSetV3 *controller.ResourceSet
+	{
+		c := v3.ResourceSetConfig{
+			ApprClient:  config.ApprClient,
+			Fs:          config.Fs,
+			G8sClient:   config.G8sClient,
+			HelmClient:  config.HelmClient,
+			K8sClient:   config.K8sClient,
+			Logger:      config.Logger,
+			ProjectName: config.ProjectName,
+		}
+
+		resourceSetV3, err = v3.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resourceSetV4 *controller.ResourceSet
+	{
+		c := v4.ResourceSetConfig{
+			ApprClient:  config.ApprClient,
+			Fs:          config.Fs,
+			G8sClient:   config.G8sClient,
+			HelmClient:  config.HelmClient,
+			K8sClient:   config.K8sClient,
+			Logger:      config.Logger,
+			ProjectName: config.ProjectName,
+		}
+
+		resourceSetV4, err = v4.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var resourceSetV5 *controller.ResourceSet
+	{
+		c := v5.ResourceSetConfig{
+			ApprClient:  config.ApprClient,
+			Fs:          config.Fs,
+			G8sClient:   config.G8sClient,
+			HelmClient:  config.HelmClient,
+			K8sClient:   config.K8sClient,
+			Logger:      config.Logger,
+			ProjectName: config.ProjectName,
+		}
+
+		resourceSetV5, err = v5.NewResourceSet(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	var chartConfigController *controller.Controller
+	{
+		c := controller.Config{
+			CRD:       v1alpha1.NewChartConfigCRD(),
+			CRDClient: crdClient,
+			Informer:  newInformer,
+			Logger:    config.Logger,
+			ResourceSets: []*controller.ResourceSet{
+				resourceSetV1,
+				resourceSetV2,
+				resourceSetV3,
+				resourceSetV4,
+				resourceSetV5,
+			},
+			RESTClient: config.G8sClient.CoreV1alpha1().RESTClient(),
+
+			Name: config.ProjectName + chartConfigControllerSuffix,
+		}
+
+		chartConfigController, err = controller.New(c)
+		if err != nil {
+			return nil, microerror.Mask(err)
+		}
+	}
+
+	c := &ChartConfig{
+		Controller: chartConfigController,
+	}
+
+	return c, nil
+}
---
+++ service/controller/chartconfig/v1/resource/chart/desired.go
@@ -0,0 +1,32 @@
+package chart
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+
+	"github.com/giantswarm/chart-operator/service/controller/chartconfig/v1/key"
+)
+
+func (r *Resource) GetDesiredState(ctx context.Context, obj interface{}) (interface{}, error) {
+	customObject, err := key.ToCustomObject(obj)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	name := key.ChartName(customObject)
+	channel := key.ChannelName(customObject)
+	releaseVersion, err := r.apprClient.GetReleaseVersion(ctx, name, channel)
+	if err != nil {
+		return nil, microerror.Mask(err)
+	}
+
+	chartState := &ChartState{
+		ChartName:      key.ChartName(customObject),
+		ChannelName:    key.ChannelName(customObject),
+		ReleaseName:    key.ReleaseName(customObject),
+		ReleaseVersion: releaseVersion,
+	}
+
+	return chartState, nil
+}
