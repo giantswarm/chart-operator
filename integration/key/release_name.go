5cc437c5428c7ebc61cd128b5bb7939f9b7fdd79
Ross Fairbanks <ross@giantswarm.io>
2019-02-13 15:33:13 +0100

    Add test steps for chart creation

--- integration/key/release_name.go
+++ integration/key/release_name.go
@@ -3,3 +3,7 @@ package key
 func ChartCustomResource() string {
 	return "apiextensions-chart-e2e-chart"
 }
+
+func TestApp() string {
+	return "test-app"
+}
--- integration/test/chart/basic/basic_test.go
+++ integration/test/chart/basic/basic_test.go
@@ -4,49 +4,59 @@ package basic
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/giantswarm/e2e-harness/pkg/release"
 	"github.com/giantswarm/e2etemplates/pkg/chartvalues"
 
-	"github.com/giantswarm/chart-operator/integration/chartconfig"
 	"github.com/giantswarm/chart-operator/integration/key"
-)
-
-const (
-	cr = "apiextensions-chart-e2e"
 )
 
 func TestChartLifecycle(t *testing.T) {
 	ctx := context.Background()
 
-	// Setup.
-	err := chartconfig.InstallResources(ctx, config)
-	if err != nil {
-		t.Fatalf("could not install resources %v", err)
-	}
+	// Test creation.
+	{
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("creating chart CR %#q", key.ChartCustomResource()))
 
-	// Install chart CR.
-	c := chartvalues.APIExtensionsChartE2EConfig{
-		Chart: chartvalues.APIExtensionsChartE2EConfigChart{
-			Name:       "kubernetes-test-app",
-			Namespace:  "giantswarm",
-			TarballURL: "https://giantswarm.github.com/sample-catalog/kubernetes-test-app-chart-0.6.8.tgz",
-		},
-		ChartOperator: chartvalues.APIExtensionsChartE2EConfigChartOperator{
-			Version: "1.0.0",
-		},
-		Namespace: "giantswarm",
-	}
+		c := chartvalues.APIExtensionsChartE2EConfig{
+			Chart: chartvalues.APIExtensionsChartE2EConfigChart{
+				Name:       key.TestApp(),
+				Namespace:  "giantswarm",
+				TarballURL: "https://giantswarm.github.com/sample-catalog/kubernetes-test-app-chart-0.6.8.tgz",
+			},
+			ChartOperator: chartvalues.APIExtensionsChartE2EConfigChartOperator{
+				Version: "1.0.0",
+			},
+			Namespace: "giantswarm",
+		}
 
-	values, err := chartvalues.NewAPIExtensionsChartE2E(c)
-	if err != nil {
-		t.Fatalf("expected %#v got %#v", nil, err)
-	}
+		chartValues, err := chartvalues.NewAPIExtensionsChartE2E(c)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
 
-	chartInfo := release.NewStableChartInfo(key.ChartCustomResource())
-	err = config.Release.EnsureInstalled(ctx, key.ChartCustomResource(), chartInfo, values)
-	if err != nil {
-		t.Fatalf("expected %#v got %#v", nil, err)
+		chartInfo := release.NewStableChartInfo(key.ChartCustomResource())
+		err = config.Release.EnsureInstalled(ctx, key.ChartCustomResource(), chartInfo, chartValues)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		err = config.Release.WaitForStatus(ctx, key.ChartCustomResource(), "DEPLOYED")
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("created chart CR %#q", key.ChartCustomResource()))
+
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("checking release %#q is deployed", key.ChartCustomResource()))
+
+		err = config.Release.WaitForStatus(ctx, key.TestApp(), "DEPLOYED")
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q is deployed", key.ChartCustomResource()))
 	}
 }
