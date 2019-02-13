441d84cc6bbdcff74fc72597150db7fc3ca233dc
Ross Fairbanks <ross@giantswarm.io>
2019-02-13 18:33:39 +0100

    Fix tests and add update checks

--- integration/test/chart/basic/basic_test.go
+++ integration/test/chart/basic/basic_test.go
@@ -24,7 +24,7 @@ func TestChartLifecycle(t *testing.T) {
 			Chart: chartvalues.APIExtensionsChartE2EConfigChart{
 				Name:       key.TestApp(),
 				Namespace:  "giantswarm",
-				TarballURL: "https://giantswarm.github.com/sample-catalog/kubernetes-test-app-chart-0.6.8.tgz",
+				TarballURL: "https://giantswarm.github.com/sample-catalog/kubernetes-test-app-chart-0.5.3.tgz",
 			},
 			ChartOperator: chartvalues.APIExtensionsChartE2EConfigChartOperator{
 				Version: "1.0.0",
@@ -60,11 +60,56 @@ func TestChartLifecycle(t *testing.T) {
 		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q is deployed", key.TestApp()))
 	}
 
+	// Test update.
+	{
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("updating chart %#q", key.ChartCustomResource()))
+
+		c := chartvalues.APIExtensionsChartE2EConfig{
+			Chart: chartvalues.APIExtensionsChartE2EConfigChart{
+				Name:      key.TestApp(),
+				Namespace: "giantswarm",
+				// Newer version of the tarball is deployed.
+				TarballURL: "https://giantswarm.github.com/sample-catalog/kubernetes-test-app-chart-0.6.8.tgz",
+			},
+			ChartOperator: chartvalues.APIExtensionsChartE2EConfigChartOperator{
+				Version: "1.0.0",
+			},
+			Namespace: "giantswarm",
+		}
+
+		updatedValues, err := chartvalues.NewAPIExtensionsChartE2E(c)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		chartInfo := release.NewStableChartInfo(key.ChartCustomResource())
+		err = config.Release.Update(ctx, key.ChartCustomResource(), chartInfo, updatedValues)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		err = config.Release.WaitForStatus(ctx, fmt.Sprintf("%s-%s", "giantswarm", key.ChartCustomResource()), "DEPLOYED")
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("updated chart %#q", key.ChartCustomResource()))
+
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("checking release %#q is updated", key.TestApp()))
+
+		err = config.Release.WaitForChartInfo(ctx, key.TestApp(), "0.6.8")
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q is updated", key.TestApp()))
+	}
+
 	// Test deletion.
 	{
 		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deleting chart %#q", key.ChartCustomResource()))
 
-		err := config.Release.Delete(ctx, fmt.Sprintf("%s-%s", "giantswarm", key.ChartCustomResource()))
+		err := config.Release.Delete(ctx, key.ChartCustomResource())
 		if err != nil {
 			t.Fatalf("expected %#v got %#v", nil, err)
 		}
