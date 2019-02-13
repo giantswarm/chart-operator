6e08f432d5080fd2416716b62ac0b536c26ca9e9
Ross Fairbanks <ross@giantswarm.io>
2019-02-13 15:32:47 +0100

    Move operator installation to setup

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -9,7 +9,10 @@ import (
 	"os"
 	"testing"
 
+	"github.com/giantswarm/microerror"
+
 	"github.com/giantswarm/chart-operator/integration/env"
+	"github.com/giantswarm/chart-operator/integration/templates"
 )
 
 func Setup(m *testing.M, config Config) {
@@ -18,15 +21,9 @@ func Setup(m *testing.M, config Config) {
 	var v int
 	var err error
 
-	err = config.Host.CreateNamespace("giantswarm")
+	err = installResources(ctx, config)
 	if err != nil {
-		config.Logger.LogCtx(ctx, "level", "error", "message", "failed to create namespace", "stack", fmt.Sprintf("%#v", err))
-		v = 1
-	}
-
-	err = config.HelmClient.EnsureTillerInstalled(ctx)
-	if err != nil {
-		config.Logger.LogCtx(ctx, "level", "error", "message", "failed to install tiller", "stack", fmt.Sprintf("%#v", err))
+		config.Logger.LogCtx(ctx, "level", "error", "message", "failed to install resources", "stack", fmt.Sprintf("%#v", err))
 		v = 1
 	}
 
@@ -48,4 +45,32 @@ func Setup(m *testing.M, config Config) {
 	}
 
 	os.Exit(v)
+}
+
+func installResources(ctx context.Context, config Config) error {
+	var err error
+
+	{
+		err = config.Host.CreateNamespace("giantswarm")
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		err = config.HelmClient.EnsureTillerInstalled(ctx)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	{
+		version := fmt.Sprintf(":%s", env.CircleSHA())
+		err = config.Host.InstallOperator("chart-operator", "chartconfig", templates.ChartOperatorValues, version)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	return nil
 }
