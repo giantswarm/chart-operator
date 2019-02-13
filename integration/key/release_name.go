918fabb3e3d5ae0d83b94a20a175f1182ca91424
Ross Fairbanks <ross@giantswarm.io>
2019-02-13 19:46:48 +0100

    Add basic test for chart CRs

--- integration/key/release_name.go
+++ integration/key/release_name.go
@@ -3,3 +3,11 @@ package key
 func ChartOperatorReleaseName() string {
 	return "chart-operator"
 }
+
+func CustomResourceReleaseName() string {
+	return "apiextensions-chart-e2e-chart"
+}
+
+func TestAppReleaseName() string {
+	return "test-app"
+}
