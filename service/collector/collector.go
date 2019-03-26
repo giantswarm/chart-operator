7ac0c1f39cd169ea88d503777485d37439a857c0
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-03-26 09:58:52 +0100

    Add tiller configuration alert to collector (#214)
    * Add chart CR support to collector
    
    * Set watch namespace in service
    
    * Remove file added in error
    
    * Address review comments
    
    * Fix log message
    
    * Add tiller configured alert
    
    * Updated wiring
    
    * Fix logging
    
    * Remove tiller configured code added in error.
    
    * Add tiller configuration alert
    
    * Enable tiller configured
    
    * Update RBAC rules
    
    * Fix merge problem
    
    * Address review comments
    
    * Update logging

--- service/collector/collector.go
+++ service/collector/collector.go
@@ -9,6 +9,7 @@ import (
 	"github.com/giantswarm/microerror"
 	"github.com/giantswarm/micrologger"
 	"github.com/prometheus/client_golang/prometheus"
+	"k8s.io/client-go/kubernetes"
 )
 
 const (
@@ -20,23 +21,30 @@ const (
 
 type Config struct {
 	G8sClient  versioned.Interface
+	K8sClient  kubernetes.Interface
 	HelmClient *helmclient.Client
 	Logger     micrologger.Logger
 
-	WatchNamespace string
+	TillerNamespace string
+	WatchNamespace  string
 }
 
 type Collector struct {
 	g8sClient  versioned.Interface
+	k8sClient  kubernetes.Interface
 	helmClient *helmclient.Client
 	logger     micrologger.Logger
 
-	watchNamespace string
+	tillerNamespace string
+	watchNamespace  string
 }
 
 func New(config Config) (*Collector, error) {
 	if config.G8sClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.G8sClient must not be empty", config)
+	}
+	if config.K8sClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.K8sClient must not be empty", config)
 	}
 	if config.HelmClient == nil {
 		return nil, microerror.Maskf(invalidConfigError, "%T.HelmClient must not be empty", config)
@@ -45,12 +53,18 @@ func New(config Config) (*Collector, error) {
 		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
 	}
 
+	if config.TillerNamespace == "" {
+		return nil, microerror.Maskf(invalidConfigError, "%T.TillerNamespace must not be empty", config)
+	}
+
 	c := &Collector{
 		g8sClient:  config.G8sClient,
+		k8sClient:  config.K8sClient,
 		helmClient: config.HelmClient,
 		logger:     config.Logger,
 
-		watchNamespace: config.WatchNamespace,
+		tillerNamespace: config.TillerNamespace,
+		watchNamespace:  config.WatchNamespace,
 	}
 
 	return c, nil
@@ -68,6 +82,7 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 
 	collectFuncs := []func(context.Context, chan<- prometheus.Metric){
 		c.collectChartConfigStatus,
+		c.collectTillerConfigured,
 		c.collectTillerReachable,
 	}
 
