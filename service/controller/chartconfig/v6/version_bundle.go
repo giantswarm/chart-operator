0c37d234306033b579e3e3752c2e855c8fe7a306
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2019-04-26 10:25:21 +0200

    V6 wip (#226)
    * v5 changelog
    
    * v6 wip
    
    * Revert "v5 changelog"
    
    This reverts commit c0e19986194375271feafca347aa6033513c45cb.

---
+++ service/controller/chartconfig/v6/version_bundle.go
@@ -0,0 +1,20 @@
+package v5
+
+import (
+	"github.com/giantswarm/versionbundle"
+)
+
+func VersionBundle() versionbundle.Bundle {
+	return versionbundle.Bundle{
+		Changelogs: []versionbundle.Changelog{
+			{
+				Component:   "chart-operator",
+				Description: "Add your changes here.",
+				Kind:        versionbundle.KindAdded,
+			},
+		},
+		Components: []versionbundle.Component{},
+		Name:       "chart-operator",
+		Version:    "0.6.0",
+	}
+}
