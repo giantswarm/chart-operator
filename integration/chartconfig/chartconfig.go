13b876b144f3bf39283489bae604c3dc648055dc
Ross Fairbanks <ross@rossfairbanks.com>
2019-10-30 15:56:17 +0100

    Remove usage of e2e-harness framework package in e2e tests

--- integration/chartconfig/chartconfig.go
+++ integration/chartconfig/chartconfig.go
@@ -6,18 +6,13 @@ import (
 	"bytes"
 	"context"
 	"html/template"
-	"log"
-	"os"
-	"strings"
 
 	"github.com/giantswarm/apprclient"
-	"github.com/giantswarm/e2e-harness/pkg/framework"
 	"github.com/giantswarm/e2etemplates/pkg/e2etemplates"
 	"github.com/giantswarm/microerror"
 	"github.com/spf13/afero"
 	"k8s.io/helm/pkg/helm"
 
-	"github.com/giantswarm/chart-operator/integration/env"
 	"github.com/giantswarm/chart-operator/integration/setup"
 )
 
@@ -52,37 +47,6 @@ func InstallResources(ctx context.Context, config setup.Config) error {
 	}
 
 	return nil
-}
-
-func VersionBundleVersion(githubToken, testedVersion string) (string, error) {
-	if githubToken == "" {
-		return "", microerror.Maskf(failedExecutionError, "env var %#q must not be empty", env.EnvVarGithubBotToken)
-	}
-	if testedVersion == "" {
-		return "", microerror.Maskf(failedExecutionError, "env var %#q must not be empty", env.EnvVarTestedVersion)
-	}
-
-	params := &framework.VBVParams{
-		Component: "chart-operator",
-		Provider:  "aws",
-		Token:     githubToken,
-		VType:     testedVersion,
-	}
-	versionBundleVersion, err := framework.GetVersionBundleVersion(params)
-	if err != nil {
-		return "", microerror.Mask(err)
-	}
-
-	if versionBundleVersion == "" {
-		if strings.ToLower(testedVersion) == "wip" {
-			log.Println("WIP version bundle version not present, exiting.")
-			os.Exit(0)
-		}
-
-		return "", microerror.Maskf(failedExecutionError, "version bundle version must not be empty")
-	}
-
-	return versionBundleVersion, nil
 }
 
 func initializeCNR(ctx context.Context, config setup.Config) error {
