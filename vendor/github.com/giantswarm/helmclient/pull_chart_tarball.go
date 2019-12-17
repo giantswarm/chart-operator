3850f95915c01ee46540de61b7ebc38f536a9997
Theo Brigitte <theo.brigitte@gmail.com>
2019-12-17 16:12:52 +0100

    more debug

--- vendor/github.com/giantswarm/helmclient/pull_chart_tarball.go
+++ vendor/github.com/giantswarm/helmclient/pull_chart_tarball.go
@@ -52,7 +52,7 @@ func (c *Client) doFile(ctx context.Context, req *http.Request) (string, error) 
 			// Github pages 404 produces full HTML page which
 			// obscures the logs.
 			if resp.StatusCode == http.StatusNotFound {
-				return backoff.Permanent(microerror.Maskf(pullChartFailedError, fmt.Sprintf("got StatusCode %d for %s %#q %s\nHeaders: %v", resp.StatusCode, req.Method, req.URL.String(), req.Proto, req.Header)))
+				return backoff.Permanent(microerror.Maskf(pullChartFailedError, fmt.Sprintf("got StatusCode %d\nRequest %s %#q %s\nHeaders: %v\n\nResponse %s %s\nHeaders: %v", resp.StatusCode, req.Method, req.URL.String(), req.Proto, req.Header, resp.Proto, resp.Status, resp.Headers)))
 			}
 			return microerror.Maskf(executionFailedError, fmt.Sprintf("got StatusCode %d for url %#q with body %s", resp.StatusCode, req.URL.String(), buf.String()))
 		}
