3c517123be04f2999a4a06c1fc9618fd2a334cc9
Ross Fairbanks <ross@rossfairbanks.com>
2019-12-18 21:47:32 +0100

    Get status from controller context

--- service/controller/chart/v1/resource/status/create.go
+++ service/controller/chart/v1/resource/status/create.go
@@ -9,6 +9,7 @@ import (
 	"github.com/giantswarm/microerror"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/giantswarm/chart-operator/service/controller/chart/v1/controllercontext"
 	"github.com/giantswarm/chart-operator/service/controller/chart/v1/key"
 )
 
@@ -16,6 +17,28 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	cr, err := key.ToCustomResource(obj)
 	if err != nil {
 		return microerror.Mask(err)
+	}
+	cc, err := controllercontext.FromContext(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	// If a reason was added to the controller context something went wrong.
+	// So we set the CR status and return early.
+	if cc.Status.Reason != "" {
+		status := v1alpha1.ChartStatus{
+			Reason: cc.Status.Reason,
+			Release: v1alpha1.ChartStatusRelease{
+				Status: cc.Status.Release.Status,
+			},
+		}
+
+		err = r.setStatus(ctx, cr, status)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		return nil
 	}
 
 	releaseName := key.ReleaseName(cr)
@@ -66,25 +89,34 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	}
 
 	if !equals(desiredStatus, key.ChartStatus(cr)) {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("setting status for release %#q status to %#q", releaseName, releaseContent.Status))
-
-		// Get chart CR again to ensure the resource version is correct.
-		currentCR, err := r.g8sClient.ApplicationV1alpha1().Charts(cr.Namespace).Get(cr.Name, metav1.GetOptions{})
+		err = r.setStatus(ctx, cr, desiredStatus)
 		if err != nil {
 			return microerror.Mask(err)
 		}
-
-		currentCR.Status = desiredStatus
-
-		_, err = r.g8sClient.ApplicationV1alpha1().Charts(cr.Namespace).UpdateStatus(currentCR)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("set status for release %#q", releaseName))
 	} else {
 		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("status for release %#q already set to %#q", releaseName, releaseContent.Status))
 	}
+
+	return nil
+}
+
+func (r *Resource) setStatus(ctx context.Context, cr v1alpha1.Chart, status v1alpha1.ChartStatus) error {
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("setting status for release %#q status to %#q", key.ReleaseName(cr), status.Release.Status))
+
+	// Get chart CR again to ensure the resource version is correct.
+	currentCR, err := r.g8sClient.ApplicationV1alpha1().Charts(cr.Namespace).Get(cr.Name, metav1.GetOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	currentCR.Status = status
+
+	_, err = r.g8sClient.ApplicationV1alpha1().Charts(cr.Namespace).UpdateStatus(currentCR)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("set status for release %#q", key.ReleaseName(cr)))
 
 	return nil
 }
