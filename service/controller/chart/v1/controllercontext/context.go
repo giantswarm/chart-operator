c3043e94903bca4bd67e8762052995c8135674db
Ross Fairbanks <rossf7@users.noreply.github.com>
2019-12-19 11:58:48 +0100

    Improve chart CR status when chart tarballs cannot be pulled (#328)
    * Add controller context for passing info to status resource
    
    * vendor: updated helmclient
    
    * Get status from controller context
    
    * Add status reasons to the controller context
    
    * vendor: updated lock
    
    * vendor: updated lock

---
+++ service/controller/chart/v1/controllercontext/context.go
@@ -0,0 +1,37 @@
+package controllercontext
+
+import (
+	"context"
+
+	"github.com/giantswarm/microerror"
+)
+
+type contextKey string
+
+const controllerKey contextKey = "controller"
+
+type Context struct {
+	Status Status
+}
+
+type Status struct {
+	Reason  string
+	Release Release
+}
+
+type Release struct {
+	Status string
+}
+
+func NewContext(ctx context.Context, c Context) context.Context {
+	return context.WithValue(ctx, controllerKey, &c)
+}
+
+func FromContext(ctx context.Context) (*Context, error) {
+	c, ok := ctx.Value(controllerKey).(*Context)
+	if !ok {
+		return nil, microerror.Maskf(notFoundError, "context key %q of type %T", controllerKey, controllerKey)
+	}
+
+	return c, nil
+}
--- vendor/github.com/giantswarm/helmclient/pull_chart_tarball.go
+++ vendor/github.com/giantswarm/helmclient/pull_chart_tarball.go
@@ -61,7 +61,7 @@ func (c *Client) doFile(ctx context.Context, req *http.Request) (string, error) 
 
 			// Github Pages 404 produces full HTML page which obscures the logs.
 			if resp.StatusCode == http.StatusNotFound {
-				return backoff.Permanent(microerror.Maskf(pullChartFailedError, fmt.Sprintf("got StatusCode %d for url %#q", resp.StatusCode, req.URL.String())))
+				return backoff.Permanent(microerror.Maskf(pullChartNotFoundError, fmt.Sprintf("got StatusCode %d for url %#q", resp.StatusCode, req.URL.String())))
 			}
 
 			return microerror.Maskf(executionFailedError, fmt.Sprintf("got StatusCode %d for url %#q with body %s", resp.StatusCode, req.URL.String(), buf.String()))
