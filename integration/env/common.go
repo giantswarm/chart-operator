11c2a741a24295aad34c34af0d968ea05b206f50
Ross Fairbanks <ross@rossfairbanks.com>
2019-12-23 19:39:20 +0000

    Delete chartconfig e2e tests

--- integration/env/common.go
+++ integration/env/common.go
@@ -5,8 +5,6 @@ package env
 import (
 	"fmt"
 	"os"
-
-	"github.com/giantswarm/chart-operator/service"
 )
 
 const (
@@ -16,27 +14,15 @@ const (
 	// EnvVarCircleSHA is the process environment variable representing the
 	// CIRCLE_SHA1 env var.
 	EnvVarCircleSHA = "CIRCLE_SHA1"
-	// EnvVarGithubBotToken is the process environment variable representing
-	// the GITHUB_BOT_TOKEN env var.
-	EnvVarGithubBotToken = "GITHUB_BOT_TOKEN"
 	// EnvVarKeepResources is the process environment variable representing the
 	// KEEP_RESOURCES env var.
 	EnvVarKeepResources = "KEEP_RESOURCES"
-	// EnvVarTestedVersion is the process environment variable representing the
-	// E2E_TESTED_VERSION env var.
-	EnvVarTestedVersion = "E2E_TESTED_VERSION"
-	// EnvVarVersionBundleVersion is the process environment variable representing the
-	// E2E_VERSION_BUNDLE_VERSION env var.
-	EnvVarVersionBundleVersion = "VERSION_BUNDLE_VERSION"
 )
 
 var (
-	circleCI             string
-	circleSHA            string
-	githubToken          string
-	keepResources        string
-	testedVersion        string
-	versionBundleVersion string
+	circleCI      string
+	circleSHA     string
+	keepResources string
 )
 
 func init() {
@@ -47,22 +33,6 @@ func init() {
 	if circleSHA == "" {
 		panic(fmt.Sprintf("env var '%s' must not be empty", EnvVarCircleSHA))
 	}
-
-	// Optional environment variables only needed for chartconfig tests.
-	githubToken = os.Getenv(EnvVarGithubBotToken)
-	testedVersion = os.Getenv(EnvVarTestedVersion)
-
-	{
-		switch testedVersion {
-		case "latest", "wip":
-			vbs := service.NewVersionBundles()
-			versionBundleVersion = vbs[len(vbs)-1].Version
-		case "previous", "current":
-			vbs := service.NewVersionBundles()
-			versionBundleVersion = vbs[len(vbs)-2].Version
-		}
-	}
-	os.Setenv(EnvVarVersionBundleVersion, VersionBundleVersion())
 }
 
 func CircleCI() string {
@@ -73,18 +43,6 @@ func CircleSHA() string {
 	return circleSHA
 }
 
-func GithubToken() string {
-	return githubToken
-}
-
 func KeepResources() string {
 	return keepResources
-}
-
-func TestedVersion() string {
-	return testedVersion
-}
-
-func VersionBundleVersion() string {
-	return versionBundleVersion
 }
--- integration/key/release_name.go
+++ integration/key/release_name.go
@@ -4,10 +4,6 @@ func ChartOperatorReleaseName() string {
 	return "chart-operator"
 }
 
-func ChartConfigReleaseName() string {
-	return "apiextensions-chart-config-e2e-chart"
-}
-
 func ChartReleaseName() string {
 	return "apiextensions-chart-e2e-chart"
 }
