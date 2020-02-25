c830b58e634f7a0e513a6c54972d4e88db744be7
Ross Fairbanks <ross@rossfairbanks.com>
2020-02-25 17:08:58 +0100

    Use client-go in basic integration test

--- integration/key/release_name.go
+++ integration/key/release_name.go
@@ -8,10 +8,6 @@ func ChartConfigReleaseName() string {
 	return "apiextensions-chart-config-e2e-chart"
 }
 
-func ChartReleaseName() string {
-	return "apiextensions-chart-e2e-chart"
-}
-
 func TestAppReleaseName() string {
 	return "test-app"
 }
