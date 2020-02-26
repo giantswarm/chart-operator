bca2de06250d7125bbb768d2d7c3d626d1364cbe
Ross Fairbanks <ross@rossfairbanks.com>
2020-02-26 13:06:07 +0100

    Push test chart to CNR

--- integration/test/chart/migration/migration_test.go
+++ integration/test/chart/migration/migration_test.go
@@ -15,6 +15,8 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
+	"github.com/giantswarm/chart-operator/integration/chartconfig"
+	"github.com/giantswarm/chart-operator/integration/cnr"
 	"github.com/giantswarm/chart-operator/integration/key"
 	"github.com/giantswarm/chart-operator/pkg/annotation"
 )
@@ -36,6 +38,28 @@ import (
 //
 func TestChartMigration(t *testing.T) {
 	ctx := context.Background()
+
+	// Push chart to CNR.
+	{
+		err := chartconfig.InstallResources(ctx, config)
+		if err != nil {
+			t.Fatalf("could not install resources %v", err)
+		}
+
+		charts := []cnr.Chart{
+			{
+				Channel: "0-7-beta",
+				Release: "0.7.0",
+				Tarball: "/e2e/fixtures/kubernetes-test-app-chart-0.7.0.tgz",
+				Name:    "test-app",
+			},
+		}
+
+		err = cnr.Push(ctx, config.K8sClients, charts)
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+	}
 
 	// Create legacy chartconfig CRD.
 	{
@@ -65,7 +89,7 @@ func TestChartMigration(t *testing.T) {
 			},
 			Spec: corev1alpha1.ChartConfigSpec{
 				Chart: corev1alpha1.ChartConfigSpecChart{
-					Channel:   "test",
+					Channel:   "0-7-beta",
 					Name:      key.TestAppReleaseName(),
 					Namespace: "giantswarm",
 					Release:   key.TestAppReleaseName(),
