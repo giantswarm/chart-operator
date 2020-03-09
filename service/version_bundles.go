85d282562c07ed7d30d28410b5d2528ab8c8dc99
Ross Fairbanks <ross@rossfairbanks.com>
2020-03-09 15:57:49 +0100

    Fixes

--- service/version_bundles.go
+++ service/version_bundles.go
@@ -5,19 +5,19 @@ import (
 
 	chartv1 "github.com/giantswarm/chart-operator/service/controller/chart/v1"
 
-	v5 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v5"
-	v6 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v6"
-	v7 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v7"
+	chartconfigv5 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v5"
+	chartconfigv6 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v6"
+	chartconfigv7 "github.com/giantswarm/chart-operator/service/controller/chartconfig/v7"
 )
 
 func NewVersionBundles() []versionbundle.Bundle {
 	var versionBundles []versionbundle.Bundle
 
 	versionBundles = append(versionBundles, chartv1.VersionBundle())
-§
-versionBundles = append(versionBundles, chartconfigv5.VersionBundle())
-versionBundles = append(versionBundles, chartconfigv6.VersionBundle())
-versionBundles = append(versionBundles, chartconfigv7.VersionBundle())
+
+	versionBundles = append(versionBundles, chartconfigv5.VersionBundle())
+	versionBundles = append(versionBundles, chartconfigv6.VersionBundle())
+	versionBundles = append(versionBundles, chartconfigv7.VersionBundle())
 
 	return versionBundles
 }
