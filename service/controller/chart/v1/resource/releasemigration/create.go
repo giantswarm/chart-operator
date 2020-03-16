f5bb870cdb9e6ebbc324a17ac62223e4d4050615
jihyuk <jhbok@giantswarm.io>
2020-03-16 16:04:52 +0100

    address review #2

--- service/controller/chart/v1/resource/releasemigration/create.go
+++ service/controller/chart/v1/resource/releasemigration/create.go
@@ -18,12 +18,12 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 		return microerror.Mask(err)
 	}
 
-	hasConfigMap, err := r.getHelmV2ConfigMaps(ctx, key.ReleaseName(cr))
+	hasConfigMap, err := r.hasHelmV2ConfigMaps(ctx, key.ReleaseName(cr))
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	hasSecret, err := r.getHelmV3Secrets(ctx, key.ReleaseName(cr), key.Namespace(cr))
+	hasSecret, err := r.hasHelmV3Secrets(ctx, key.ReleaseName(cr), key.Namespace(cr))
 	if err != nil {
 		return microerror.Mask(err)
 	}
@@ -31,7 +31,7 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	// If Helm v2 release configmap had not been deleted and Helm v3 release secret is there,
 	// It means helm release migration is in progress.
 	if hasConfigMap && hasSecret {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q is under helmV3 migration", key.ReleaseName(cr)))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q helmV3 migration in progress", key.ReleaseName(cr)))
 		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
 		reconciliationcanceledcontext.SetCanceled(ctx)
 		return nil
@@ -40,23 +40,23 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 	// If Helm v2 release configmap had not been deleted and Helm v3 release secret was not created,
 	// It means helm v3 release migration is not started.
 	if hasConfigMap && !hasSecret {
-		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q is not in under helmV3 migration yet", key.ReleaseName(cr)))
+		r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("release %#q helmV3 migration not started", key.ReleaseName(cr)))
 		r.logger.LogCtx(ctx, "level", "debug", "message", "canceling reconciliation")
 		reconciliationcanceledcontext.SetCanceled(ctx)
 		return nil
 	}
 
-	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("no pending migration on release %#q", key.ReleaseName(cr)))
+	r.logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("no pending migration for release %#q", key.ReleaseName(cr)))
 
 	return nil
 }
 
-func (r *Resource) getHelmV2ConfigMaps(ctx context.Context, releaseName string) (bool, error) {
+func (r *Resource) hasHelmV2ConfigMaps(ctx context.Context, releaseName string) (bool, error) {
 	lo := metav1.ListOptions{
 		LabelSelector: fmt.Sprintf("%s=%s,%s=%s", "NAME", releaseName, "OWNER", "TILLER"),
 	}
 
-	// Check whether it keep helm2 release configMaps
+	// Check whether helm 2 release configMaps still exist.
 	cms, err := r.k8sClient.CoreV1().ConfigMaps(r.tillerNamespace).List(lo)
 	if err != nil {
 		return false, microerror.Mask(err)
@@ -65,12 +65,12 @@ func (r *Resource) getHelmV2ConfigMaps(ctx context.Context, releaseName string) 
 	return len(cms.Items) > 0, nil
 }
 
-func (r *Resource) getHelmV3Secrets(ctx context.Context, releaseName, releaseNamespace string) (bool, error) {
+func (r *Resource) hasHelmV3Secrets(ctx context.Context, releaseName, releaseNamespace string) (bool, error) {
 	lo := metav1.ListOptions{
 		LabelSelector: fmt.Sprintf("%s=%s,%s=%s", "name", releaseName, "owner", "helm"),
 	}
 
-	// Check whether it keep helm3 release secrets
+	// Check whether helm 3 release secret exists.
 	secrets, err := r.k8sClient.CoreV1().Secrets(releaseNamespace).List(lo)
 	if err != nil {
 		return false, microerror.Mask(err)
