81b2ec577342bf012712ddb2f7ba81eef258b98e
Ross Fairbanks <ross@rossfairbanks.com>
2020-03-17 13:54:09 +0100

    compiling

--- integration/test/chart/basic/basic_test.go
+++ integration/test/chart/basic/basic_test.go
@@ -100,7 +100,7 @@ func TestChartLifecycle(t *testing.T) {
 
 		config.Logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("checking release %#q is updated", key.TestAppReleaseName()))
 
-		err = config.Release.WaitForChartInfo(ctx, key.Namespace(), key.TestAppReleaseName(), "0.1.1")
+		err = config.Release.WaitForChartInfo(ctx, key.TestAppReleaseName(), "0.1.1")
 		if err != nil {
 			t.Fatalf("expected %#v got %#v", nil, err)
 		}
