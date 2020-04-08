9ea78e193272868219fee6a8f6b59b950a701ece
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-04-08 14:29:59 +0200

    Add missing values to fix integration tests (#403)

--- integration/templates/chart_operator_values.go
+++ integration/templates/chart_operator_values.go
@@ -7,4 +7,12 @@ const ChartOperatorValues = `cnr:
   address: http://cnr-server:5000
 clusterDNSIP: 10.96.0.10
 e2e: true
+
+Installation:
+  V1:
+    Helm:
+      HTTP:
+        ClientTimeout: 30s
+    Registry:
+      Domain: quay.io
 `
