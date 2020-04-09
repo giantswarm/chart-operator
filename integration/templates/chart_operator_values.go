8b5a751dfb785618d9aa920a82f544d2c21bf493
Ross Fairbanks <ross@rossfairbanks.com>
2020-04-09 16:49:14 +0200

    Make HTTP client timeout configurable for helmclient for AWS China

--- integration/templates/chart_operator_values.go
+++ integration/templates/chart_operator_values.go
@@ -7,4 +7,12 @@ const ChartOperatorValues = `cnr:
   address: http://cnr-server:5000
 clusterDNSIP: 10.96.0.10
 e2e: false
+
+Installation:
+  V1:
+    Helm:
+      HTTP:
+        ClientTimeout: 5s
+    Registry:
+      Domain: quay.io
 `
