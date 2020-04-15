039d1d62f4ee3e5a35366be97c06343c590f44b4
Ross Fairbanks <rossf7@users.noreply.github.com>
2020-04-15 19:00:09 +0200

    helm3: Only update failed releases if the chart values or version has changed (#426)
    * Only update failed releases if the chart values or version has changed
    
    * Always set checksum annotation for chart CRs
    
    * Move labels and annotations from key to pkg
    
    * Refactor isReleaseFailed and isReleaseModified
    
    * Fix logic and add godoc
    
    * Use constant from helmclient
    
    * Update version to fix CI
    
    * compiling

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "chart-operator"
 	source      = "https://github.com/giantswarm/chart-operator"
-	version     = "1.0.0-alpha.1-dev"
+	version     = "0.12.5-dev"
 )
 
 // ChartVersion is fixed for chart CRs. This is because they exist in both
