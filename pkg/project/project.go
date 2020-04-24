0aaef96d8bb162e8eb8367eda380d10a0d4bac24
Ross Fairbanks <ross@rossfairbanks.com>
2020-04-24 14:07:42 +0200

    Align version with master

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "chart-operator"
 	source      = "https://github.com/giantswarm/chart-operator"
-	version     = "0.12.5-dev"
+	version     = "0.13.0"
 )
 
 // ChartVersion is fixed for chart CRs. This is because they exist in both
