cbacf365e9633281909eca61992c1fcd99927396
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-05-06 15:43:45 +0200

    update to 1.0.0 (#449)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "chart-operator"
 	source      = "https://github.com/giantswarm/chart-operator"
-	version     = "0.13.1-dev"
+	version     = "1.0.0-dev"
 )
 
 // ChartVersion is fixed for chart CRs. This is because they exist in both
