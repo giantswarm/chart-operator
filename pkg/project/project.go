faf49518a030443ed68a7cde379665604fdc3358
GitHub Action <action@github.com>
2020-08-18 09:54:48 +0000

    Bump version to 2.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "chart-operator"
 	source      = "https://github.com/giantswarm/chart-operator"
-	version     = "2.1.0"
+	version     = "2.1.1-dev"
 )
 
 // ChartVersion is fixed for chart CRs. This is because they exist in both
