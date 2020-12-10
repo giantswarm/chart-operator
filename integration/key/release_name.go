9c139f859314b04c0b88013953f331509b6a0bcb
Jihyuk Bok <jhbok@giantswarm.io>
2020-12-10 11:13:40 +0100

    adding integration testing for helm values

--- integration/key/release_name.go
+++ integration/key/release_name.go
@@ -2,10 +2,6 @@
 
 package key
 
-func ReleaseName() string {
-	return "chart-operator"
-}
-
 func TestAppReleaseName() string {
 	return "test-app"
 }
--- integration/test/chart/basic/basic_test.go
+++ integration/test/chart/basic/basic_test.go
@@ -4,6 +4,7 @@ package basic
 
 import (
 	"context"
+	"reflect"
 	"testing"
 	"time"
 
@@ -12,9 +13,49 @@ import (
 	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/helmclient/v3/pkg/helmclient"
 	"github.com/giantswarm/microerror"
+	"github.com/google/go-cmp/cmp"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/yaml"
 
 	"github.com/giantswarm/chart-operator/v2/integration/key"
+)
+
+const (
+	configMapName = "test-app-configmap"
+	secretName    = "test-app-configmap"
+
+	configmapValue = `
+v1:
+  username: admin
+  host:
+    url: quay.io
+  memory_in_gb: 0
+  threshold: 2.17
+replicas: 3
+`
+	secretValue = `
+v1:
+  username: admin
+  host: 
+    secret: 
+      authToken: xer32wnq
+  githubToken: nnbhwk1dk
+  memory_in_gb: 3.14
+`
+
+	mergedValue = `
+v1:
+  username: admin
+  host:
+    url: quay.io
+    secret: 
+      authToken: xer32wnq
+  githubToken: nnbhwk1dk
+  memory_in_gb: 3.14
+  threshold: 2.17
+replicas: 3
+`
 )
 
 // TestChartLifecycle tests a Helm release can be created, updated and deleted
@@ -32,6 +73,47 @@ import (
 func TestChartLifecycle(t *testing.T) {
 	ctx := context.Background()
 
+	// creating dependant configmap & secret
+	{
+		config.Logger.Debugf(ctx, "creating configmap %#q", configMapName)
+
+		cr := &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      configMapName,
+				Namespace: key.Namespace(),
+			},
+			Data: map[string]string{
+				"values": configmapValue,
+			},
+		}
+
+		_, err := config.K8sClients.K8sClient().CoreV1().ConfigMaps(key.Namespace()).Create(ctx, cr, metav1.CreateOptions{})
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		config.Logger.Debugf(ctx, "creating configmap %#q", configMapName)
+
+		config.Logger.Debugf(ctx, "creating secret %#q", secretName)
+
+		secret := &corev1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      secretName,
+				Namespace: key.Namespace(),
+			},
+			StringData: map[string]string{
+				"values": secretValue,
+			},
+		}
+
+		_, err = config.K8sClients.K8sClient().CoreV1().Secrets(key.Namespace()).Create(ctx, secret, metav1.CreateOptions{})
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		config.Logger.Debugf(ctx, "creating secret %#q", secretName)
+	}
+
 	// Test creation.
 	{
 		config.Logger.Debugf(ctx, "creating chart %#q", key.TestAppReleaseName())
@@ -45,8 +127,18 @@ func TestChartLifecycle(t *testing.T) {
 				},
 			},
 			Spec: v1alpha1.ChartSpec{
-				Name:       key.TestAppReleaseName(),
-				Namespace:  key.Namespace(),
+				Name:      key.TestAppReleaseName(),
+				Namespace: key.Namespace(),
+				Config: v1alpha1.ChartSpecConfig{
+					ConfigMap: v1alpha1.ChartSpecConfigConfigMap{
+						Name:      configMapName,
+						Namespace: key.Namespace(),
+					},
+					Secret: v1alpha1.ChartSpecConfigSecret{
+						Name:      secretName,
+						Namespace: key.Namespace(),
+					},
+				},
 				TarballURL: "https://giantswarm.github.io/default-catalog/test-app-0.1.0.tgz",
 				Version:    "0.1.0",
 			},
@@ -119,6 +211,30 @@ func TestChartLifecycle(t *testing.T) {
 		}
 
 		config.Logger.Debugf(ctx, "release %#q is updated", key.TestAppReleaseName())
+	}
+
+	// Check all values are merged correctly
+	{
+		config.Logger.Debugf(ctx, "comparing helm values %#q", key.TestAppReleaseName())
+
+		content, err := config.HelmClient.GetReleaseContent(ctx, key.Namespace(), key.TestAppReleaseName())
+		if err != nil {
+			t.Fatalf("expected %#v got %#v", nil, err)
+		}
+
+		var mergedMap map[string]interface{}
+		{
+			err = yaml.Unmarshal([]byte(mergedValue), &mergedMap)
+			if err != nil {
+				t.Fatalf("expected %#v got %#v", nil, err)
+			}
+		}
+
+		if !reflect.DeepEqual(content.Values, mergedMap) {
+			t.Fatalf("expected same got %s", cmp.Diff(content.Values, mergedMap))
+		}
+
+		config.Logger.Debugf(ctx, "compared helm values %#q", key.TestAppReleaseName())
 	}
 
 	// Test deletion.
