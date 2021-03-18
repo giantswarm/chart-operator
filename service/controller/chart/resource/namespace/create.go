16a34aa9e8a42772c0426fada1870d15b4f974c1
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2021-03-18 14:41:43 +0100

    Update Namespace's metadata as chart CR (#716)
    * update go.mod
    
    * update labels/annotations as app spec
    
    * avoid assigning nil
    
    * avoid assigning nil
    
    * nil map
    
    * add logs
    
    * CHANGELOG.md

--- service/controller/chart/resource/namespace/create.go
+++ service/controller/chart/resource/namespace/create.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/giantswarm/apiextensions/v3/pkg/apis/application/v1alpha1"
 	"github.com/giantswarm/apiextensions/v3/pkg/label"
 	"github.com/giantswarm/microerror"
 	corev1 "k8s.io/api/core/v1"
@@ -22,12 +23,17 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 
 	ns := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: key.Namespace(cr),
-			Labels: map[string]string{
-				label.ManagedBy: project.Name(),
-			},
+			Annotations: key.NamespaceAnnotations(cr),
+			Labels:      key.NamespaceLabels(cr),
+			Name:        key.Namespace(cr),
 		},
 	}
+
+	if ns.Labels == nil {
+		ns.Labels = map[string]string{}
+	}
+
+	ns.Labels[label.ManagedBy] = project.Name()
 
 	r.logger.Debugf(ctx, "creating namespace %#q", ns.Name)
 
@@ -49,12 +55,65 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 
 	if apierrors.IsAlreadyExists(err) {
 		r.logger.Debugf(ctx, "already created namespace %#q", key.Namespace(cr))
+
+		err = r.ensureNamespaceUpdated(ctx, cr)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
 		return nil
 	} else if err != nil {
 		return microerror.Mask(err)
 	}
 
 	r.logger.Debugf(ctx, "created namespace %#q", key.Namespace(cr))
+
+	return nil
+}
+
+func (r *Resource) ensureNamespaceUpdated(ctx context.Context, cr v1alpha1.Chart) error {
+	namespace, err := r.k8sClient.CoreV1().Namespaces().Get(ctx, key.Namespace(cr), metav1.GetOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	updated := true
+
+	if namespace.GetLabels() == nil {
+		namespace.Labels = map[string]string{}
+	}
+
+	for k, v := range key.NamespaceLabels(cr) {
+		if namespace.GetLabels()[k] != v {
+			namespace.GetLabels()[k] = v
+			updated = false
+		}
+	}
+
+	if namespace.GetAnnotations() == nil {
+		namespace.Annotations = map[string]string{}
+	}
+
+	for k, v := range key.NamespaceAnnotations(cr) {
+		if namespace.GetAnnotations()[k] != v {
+			namespace.GetAnnotations()[k] = v
+			updated = false
+		}
+	}
+
+	if updated {
+		// no-op
+		return nil
+	}
+
+	r.logger.Debugf(ctx, "updating namespace %#q", namespace.Name)
+
+	_, err = r.k8sClient.CoreV1().Namespaces().Update(ctx, namespace, metav1.UpdateOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	r.logger.Debugf(ctx, "updated namespace %#q", namespace.Name)
 
 	return nil
 }
