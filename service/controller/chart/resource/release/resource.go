53097b36b9441f2eba0e0c5c11522f60b6c6bd56
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2021-04-30 19:57:21 +0200

    Handling unknown helm errors (#745)
    * add unknown reasons
    
    * delete comments
    
    * Update service/controller/chart/resource/release/resource.go
    
    Co-authored-by: Ross Fairbanks <rossf7@users.noreply.github.com>
    
    Co-authored-by: Ross Fairbanks <rossf7@users.noreply.github.com>

--- service/controller/chart/resource/release/resource.go
+++ service/controller/chart/resource/release/resource.go
@@ -48,6 +48,9 @@ const (
 	// not apply for failures.
 	releaseFailedMaxAttempts = 5
 
+	// unknownError when a release fails for unknown reasons.
+	unknownError = "unknown-error"
+
 	// validationFailedStatus is set in the CR status when it failed to pass
 	// OpenAPI validation on release manifest.
 	validationFailedStatus = "validation-failed"
