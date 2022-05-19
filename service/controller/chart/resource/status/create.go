3f9c7c05c6c83c398c8dff235891001328af66b2
Laszlo Uveges <laszlo@giantswarm.io>
2022-05-19 13:17:36 +0200

    Add controller context status to Chart CR status in more cases where it makes sense

--- service/controller/chart/resource/status/create.go
+++ service/controller/chart/resource/status/create.go
@@ -85,6 +85,10 @@ func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
 				} else {
 					reason = releaseContent.Description // and again we reuse the actual Helm release state
 				}
+			} else if cc.Status.Reason != "" {
+				// It can be that there was previous successful deployment and that is the current status of the Helm release
+				// but a follow-up deployment failed for another version, so we want to set a reason indicating the issue
+				reason = fmt.Sprintf("Deployment failed for %#q with %#q: %#q", cr.Spec.Version, cc.Status.Release.Status, cc.Status.Reason)
 			}
 		}
 	}
