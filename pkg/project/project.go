e492a689756260c8121c8e8b9e91e109e4481135
GitHub Action <action@github.com>
2022-05-19 13:17:45 +0000

    Bump version to 2.21.2-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "chart-operator"
 	source      = "https://github.com/giantswarm/chart-operator"
-	version     = "2.21.1"
+	version     = "2.21.2-dev"
 )
 
 const (
