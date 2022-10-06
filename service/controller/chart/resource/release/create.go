10983a9f97c2b0f26d568fcd3ec4fb8031376ffb
Łukasz Jakimczuk <lukasz.j@giantswarm.io>
2022-10-06 12:06:52 +0200

    Do not use chan

--- service/controller/chart/resource/release/create.go
+++ service/controller/chart/resource/release/create.go
@@ -93,6 +93,8 @@ func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange inte
 	// If we do timeout the install will continue in the background.
 	// We will check the progress in the next reconciliation loop.
 	go func() {
+		var e error
+
 		defer close(ch)
 
 		if skipCRDs {
@@ -111,19 +113,19 @@ func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange inte
 
 		// We need to pass the ValueOverrides option to make the install process
 		// use the default values and prevent errors on nested values.
-		err := hc.InstallReleaseFromTarball(ctx, tarballPath, ns, releaseState.Values, iOpts)
+		e = hc.InstallReleaseFromTarball(ctx, tarballPath, ns, releaseState.Values, iOpts)
 
 		// We check the error here to return early if installation failed. There is no point
 		// in upgrading in such scenario.
-		if err != nil {
-			ch <- err
+		if e != nil {
+			err = e
 			return
 		}
 
 		// Load the chart to get its annotations and verify it is a subject to
 		// internal upgrade procedure.
-		chart, err := hc.LoadChart(ctx, tarballPath)
-		if err != nil {
+		chart, e := hc.LoadChart(ctx, tarballPath)
+		if e != nil {
 			r.logger.Errorf(ctx, err, "loading chart %#q failed on internal upgrade", tarballPath)
 			return
 		}
@@ -145,20 +147,20 @@ func (r *Resource) ApplyCreateChange(ctx context.Context, obj, createChange inte
 		}
 
 		r.logger.Debugf(ctx, "doing internal upgrade for release %#q", releaseState.Name)
-		err = hc.UpdateReleaseFromTarball(ctx,
+		e = hc.UpdateReleaseFromTarball(ctx,
 			tarballPath,
 			ns,
 			releaseState.Name,
 			releaseState.Values,
 			uOpts)
 
-		if err != nil {
-			ch <- err
+		if e != nil {
+			err = e
 		}
 	}()
 
 	select {
-	case err = <-ch:
+	case <-ch:
 		// Fall through.
 	case <-time.After(r.k8sWaitTimeout):
 		r.logger.Debugf(ctx, "waited for %d secs. release still being created", int64(r.k8sWaitTimeout.Seconds()))
