3d46abf8e8fab3f9887181b9d37e003ee4951e06
taylorbot <dev@giantswarm.io>
2023-09-26 13:59:45 +0000

    Release v2.35.2

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "chart-operator"
 	source      = "https://github.com/giantswarm/chart-operator"
-	version     = "2.35.2-dev"
+	version     = "2.35.2"
 )
 
 const (
