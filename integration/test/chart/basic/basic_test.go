b1bf874605c47c1a50360b209bbf8886134122ae
Laszlo Uveges <laszlo@giantswarm.io>
2023-10-04 16:06:42 +0200

    Fix version bumps missed by the automation

--- integration/test/chart/basic/basic_test.go
+++ integration/test/chart/basic/basic_test.go
@@ -20,7 +20,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/yaml"
 
-	"github.com/giantswarm/chart-operator/v2/integration/key"
+	"github.com/giantswarm/chart-operator/v3/integration/key"
 )
 
 const (
